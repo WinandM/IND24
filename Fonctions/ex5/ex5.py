69dbba5730205b3fcb02a512c6012f0edb1fd18a
WinandM <m.winand@ind-philippeville.be>
2024-09-10 14:34:14 +0200

    ex5

---
+++ Fonctions/ex5/ex5.py
@@ -0,0 +1,9 @@
+def aire_Rect(longueur, largeur):
+    return longueur * largeur
+
+def aire_Carre(cote):
+    return aire_Rect(cote, cote)
+
+def aire_losange(diag1, diag2):
+    return aire_Rect(diag1, diag2)/2
+
