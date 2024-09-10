28f512e8c86260f7d0b623aa925864b9af74deb5
WinandM <m.winand@ind-philippeville.be>
2024-09-10 14:38:37 +0200

    ex5

--- Fonctions/ex5/ex5.py
+++ Fonctions/ex5/ex5.py
@@ -7,3 +7,5 @@ def aire_Carre(cote):
 def aire_losange(diag1, diag2):
     return aire_Rect(diag1, diag2)/2
 
+#cette ligne est un troll... ne faites pas ca a la maison.
+print(f"l'aire du rectangle est {aire_Rect(float(input("veuillez entrer la longueur du rectangle")), float(input("Veuillez entrer la largeur du rectangle")))}")
