10b989a844e1d5a0927b35368066c12fe1cccefa
mikross <mikross20@hotmail.com>
2024-09-14 23:58:01 +0200

    test

--- Fonctions/ex5/ex5.py
+++ Fonctions/ex5/ex5.py
@@ -8,4 +8,7 @@ def aire_losange(diag1, diag2):
     return aire_Rect(diag1, diag2)/2
 
 #cette ligne est un troll... ne faites pas ca a la maison.
-print(f"l'aire du rectangle est {aire_Rect(float(input("veuillez entrer la longueur du rectangle")), float(input("Veuillez entrer la largeur du rectangle")))}")
+longueur = float(input("veuillez entrer la longueur du rectangle")) 
+largeur= float(input("Veuillez entrer la largeur du rectangle"))
+aire = aire_Rect(longueur, largeur)
+print(f"l'aire du rectangle est {aire}")
