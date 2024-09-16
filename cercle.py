ffc6e1f31c3f9f568ff94a1875887b205865ceff
WinandM <m.winand@ind-philippeville.be>
2024-09-16 09:37:35 +0200

    ok

--- cercle.py
+++ cercle.py
@@ -9,4 +9,5 @@ def aireCercle(rayon):
 rayon = float(input("Veuillez entrer le rayon: "))
 perimetre = perimetreCercle(rayon)
 aire = aireCercle(rayon)
+
 print(f"Le périmètre d'un cercle de {rayon} de rayon est {perimetre:.2f} et son aire de {aire:.2f}")
