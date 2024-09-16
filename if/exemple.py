365b5ec377e6a2e2a6fd05c9175987cb1d894c5d
WinandM <m.winand@ind-philippeville.be>
2024-09-16 12:58:42 +0200

    Commit très important

---
+++ if/exemple.py
@@ -0,0 +1,10 @@
+score = 90
+victoire = True
+
+if score >= 100 and victoire:
+    print("Félicitations, tu es un champion !")
+elif score >= 100 and not victoire:
+    print("Bien joué, mais tu n'as pas gagné cette fois.")
+else:
+    print("Continue d'essayer, tu y arriveras !")
+    
